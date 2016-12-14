584f229914b7d2fb739f1dcdbf01dd7864bf7f2d
Nick Pether <npether@stanford.edu>
2016-12-14 15:06:34 -0800

    sending pdfPages for train 2 over to Todd

--- FeatureLab.py
+++ FeatureLab.py
@@ -26,25 +26,27 @@ def display(img):
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-def isCharacterSized(box):
-	return (w < WIDTH_MAX and h < HEIGHT_MAX and w > WIDTH_MIN and h > HEIGHT_MIN)
-
 
 display(img)
 """
-Find the contours
+Find the character boxes
 """
+def isCharacterSized(box): 
+	x,y,w,h = box
+	return (w < WIDTH_MAX and h < HEIGHT_MAX and w > WIDTH_MIN and h > HEIGHT_MIN)
+
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray,(5,5),0)
 thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
 contours, _ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
-charBoxes = [cv2.boundingRect(contour) for contour in contours]
+charBoxes = [cv2.boundingRect(contour) for contour in contours] #x,y,w,h form
 charBoxes = [c for c in charBoxes if isCharacterSized(c)]
+charBoxes = [(c[0],c[1], c[0]+c[2],c[1]+c[3]) for c in charBoxes] #x1,y2,x2,y2 form
+
 for c in charBoxes:
-	x,y,w,h = c
-	x2 = x+w
-	y2 = y+h
+	x,y,x2,y2 = c
 	cv2.rectangle(img, (x,y),(x2,y2), (0,255,0), 3)
+
 
 display(img)
